dbdd328ef2841bf09e455d555ee53f1f14de5dfe
TomasVotruba <tomas.vot@gmail.com>
2020-08-03 22:20:27 +0200

    cleanup repositories

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,28 +4,27 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Checkbox;
 use App\Entity\Checklist;
 use App\Entity\Project;
 use App\Form\ProjectFormType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CheckboxRepository;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\RouterInterface;
 
 final class ProjectController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
+    private CheckboxRepository $checkboxRepository;
 
-    private RouterInterface $router;
+    private ProjectRepository $projectRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, RouterInterface $router)
+    public function __construct(CheckboxRepository $checkboxRepository, ProjectRepository $projectRepository)
     {
-        $this->entityManager = $entityManager;
-        $this->router = $router;
+        $this->checkboxRepository = $checkboxRepository;
+        $this->projectRepository = $projectRepository;
     }
 
     /**
@@ -50,8 +49,8 @@ final class ProjectController extends AbstractController
      */
     public function show(Project $project): Response
     {
-        $currentFramework = $project->getCurrentFramework();
-        $checkboxes = $this->entityManager->getRepository(Checkbox::class)->findByFramework($currentFramework);
+        $currentFramework = (string) $project->getCurrentFramework();
+        $checkboxes = $this->checkboxRepository->findByFramework($currentFramework);
 
         return $this->render('project/show.html.twig', [
             'project' => $project,
@@ -61,15 +60,12 @@ final class ProjectController extends AbstractController
 
     private function processFormRequest(Project $project): RedirectResponse
     {
-        /** @var Checklist $checklist */
         $checklist = new Checklist();
         $checklist->setProject($project);
         $project->addChecklist($checklist);
 
-        $this->entityManager->persist($checklist);
-        $this->entityManager->persist($project);
-        $this->entityManager->flush();
+        $this->projectRepository->save($project);
 
-        return new RedirectResponse($this->router->generate('project.show', ['id' => $project->getId()]));
+        return $this->redirectToRoute('project.show', ['id' => $project->getId()]);
     }
 }
