916ff9f898c19d13e708ebec548337aa91461e69
TomasVotruba <tomas.vot@gmail.com>
2020-08-08 18:51:14 +0200

    twig: use raw twitter bootstrap 4 tempalte (to be updated to 5)

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('twig', ['default_path' => '%kernel.project_dir%/templates']);
+    $containerConfigurator->extension('twig', [
+        'default_path' => '%kernel.project_dir%/templates'
+    ]);
 
     $containerConfigurator->extension('twig', [
-        'form_themes' => ['bootstrap_4_layout.html.twig'],
+        'form_themes' => [
+            'form/bootstrap_5_layout.html.twig'
+        ],
     ]);
 };
