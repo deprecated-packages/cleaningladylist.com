50ae9169b359882c39a2fc7830a5b2b87e1038df
TomasVotruba <tomas.vot@gmail.com>
2020-08-20 17:24:47 +0200

    fix dataase load

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -16,18 +16,13 @@ if [ "$1" = 'apache2-foreground' ] || [ "$1" = 'bin/console' ] || [ "$1" = 'php'
 
     ## Wait until database connection is ready
     # @todo if sleep is not enough add custom "bin/console check:database" that already knows the databse exists
-
-    ## Update DB - broken for now: https://github.com/rectorphp/cleaningladylist.com/runs/1007822678
-    # possibly add to run.sh script on the server
-    # sleep 5
-    # php bin/console doctrine:schema:update --dump-sql --force
+    sleep 5
 
     ## Create DB
-    # can be repalced in docker-compose MYSQL_DATABAE
-    # php bin/console doctrine:database:create --if-not-exists
+    # database is created inside automatically by using "services > mysql > environment > MYSQL_DATABASE" in docker-composer.yml, see https://hub.docker.com/_/mysql
 
     ## Update DB
-    # php bin/console doctrine:schema:update --dump-sql --force
+    php bin/console doctrine:schema:update --dump-sql --force
 
     # Permissions hack because setfacl does not work on Mac and Windows
     chown -R www-data var
